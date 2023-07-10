227c2c201639f7c5ccfc00e079d63681f30b5c2c
Liam Davis <liamd416@gmail.com>
2023-07-10 13:56:20 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,118 @@
+const numbersQuiz = [
+    { //1
+        question_title:""
+        correct:"A",
+        option: {
+            A:
+            B:
+            C:
+            D:
+        }
+    },
+    { //2
+        question_title:""
+        correct:"A",
+        option: {
+            A:
+            B:
+            C:
+            D:
+        }
+    },
+    { //3
+        question_title:""
+        correct:"A",
+        option: {
+            A:
+            B:
+            C:
+            D:
+        }
+    },
+    { //4
+        question_title:""
+        correct:"A",
+        option: {
+            A:
+            B:
+            C:
+            D:
+        }
+    },
+    { //5
+        question_title:""
+        correct:"A",
+        option: {
+            A:
+            B:
+            C:
+            D:
+        }
+    },
+    { //6
+        question_title:""
+        correct:"A",
+        option: {
+            A:
+            B:
+            C:
+            D:
+        }
+    },
+    { //7
+        question_title:""
+        correct:"A",
+        option: {
+            A:
+            B:
+            C:
+            D:
+        }
+    },
+    { //8
+        question_title:""
+        correct:"A",
+        option: {
+            A:
+            B:
+            C:
+            D:
+        }
+    },
+    { //9
+        question_title:"",
+        correct:"A",
+        option: {
+            A:
+            B:
+            C:
+            D:
+        }
+    }
+]
+
+const marvelQuiz = [
+    {
+        question_title:""
+        correct:"A",
+        option: {
+            A:
+            B:
+            C:
+            D:
+        }
+    }
+]
+
+const starwarsQuiz = [
+    {
+        question_title:""
+        correct:"A",
+        option: {
+            A:
+            B:
+            C:
+            D:
+        }
+    }
+]
